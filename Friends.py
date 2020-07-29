427c1fbcf77809f500e619e169ab8d9224e9d18d
Samuel McCulloch <samgoat@Samuel-iMac>
2020-07-29 21:01:02 +0800

    the button clicking for add friend now works

--- Friends.py
+++ Friends.py
@@ -8,5 +8,5 @@ def addFriends(driver):
     driver.get("https://www.facebook.com/?sk=ff")
     time.sleep(4)
 
-    element = driver.find_element_by_id("contains(as a friend)")
+    element = driver.find_element_by_xpath('//*[@id="u_0_11"]/button[1]')
     element.click()
