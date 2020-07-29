e7660124872b861bafa84f497bf6df27d90e7fb6
Samuel McCulloch <samgoat@Samuel-iMac>
2020-07-29 21:06:46 +0800

    added multiple friend adding[B

--- JeremiahWashington.py
+++ JeremiahWashington.py
@@ -7,6 +7,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = logIO.login()
-Friends.addFriends(driver)
+for x in range(10):
+    Friends.addFriends(driver)
+    time.sleep(3)
 logIO.logout(driver)
 # example            new = driver.page_source
