b843619a0dc0e62b0fb323b4546bf3af7eb11305
Moritz Bergemann <moritz.bergemann@gmail.com>
2020-07-29 20:43:56 +0800

    removed notifications

--- Friends.py
+++ Friends.py
@@ -7,6 +7,6 @@ from selenium.webdriver.common.keys import Keys
 def addFriends(driver):
     driver.get("https://www.facebook.com/?sk=ff")
     time.sleep(4)
+
     element = driver.find_element_by_id("contains(as a friend)")
     element.click()
-	
--- logIO.py
+++ logIO.py
@@ -3,16 +3,27 @@ import sys
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 # This automatically gets the right webdriver without needing to do wonky install stuff
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 def login():
+    option = Options()
+    option.add_argument("--disable-infobars")
+    option.add_argument("start-maximized")
+    option.add_argument("--disable-extensions")
+
+    # Pass the argument 1 to allow and 2 to block
+    option.add_experimental_option("prefs", { 
+        "profile.default_content_setting_values.notifications": 1 
+    })
+
     #opens facebook website
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver = webdriver.Chrome(chrome_options=option, executable_path=ChromeDriverManager().install())
     driver.get("https://www.facebook.com")
-    
+
     old = driver.page_source
 
     #enters details
@@ -31,6 +42,7 @@ def login():
     while(new == old):
         time.sleep(2)
         new = driver.page_source
+        print("Page has changed :)")
     return driver
 
 def logout(driver):
